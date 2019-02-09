56157eca8a7296dc3da8e3b33ae9f86357a3e5f0
stedo <stefan.doverud@gmail.com>
2019-02-09 20:21:40 +0100

    debugging / noob

--- main.py
+++ main.py
@@ -31,36 +31,35 @@ class MyClient(discord.Client):
 
     async def on_message(self, message):
         if message.content.startswith('!lunch'):
-            time_delta = datetime.datetime.now() - self.last_lunch_message_sent
-            if time_delta.days > 0 or time_delta.seconds >= 12 * 60 * 60:
-                self.last_lunch_message_sent = datetime.datetime.now()
-                channel = self.get_channel(540608386299985940)
-                await channel.send("""
-:white_check_mark: Ja
-:no_entry: Nej
-<:mk:541268624392978442> Matkultur
-:hot_pepper: Chili & Lime
-:pizza: La Fontana 
-:house: Husman
-:sushi: Yatai
-:green_salad: Brödernas
-:golf: Golfen
-:office: Collegium
-:cityscape: LPS
-:grey_question:  Annat, kommentera!""")
-            else:
-                await message.author.send("DOOF! Lunchvote redan uppe.")
+            await self.send_lunch_message()
 
     async def send_lunch_message(self):
-        channel = self.get_channel(540608386299985940)
-        await channel.send(datetime.datetime.now().hour)
+        time_delta = datetime.datetime.now() - self.last_lunch_message_sent
+        if time_delta.days > 0 or time_delta.seconds >= 12 * 60 * 60:
+            self.last_lunch_message_sent = datetime.datetime.now()
+            channel = self.get_channel(540608386299985940)
+            await channel.send("""
+        :white_check_mark: Ja
+        :no_entry: Nej
+        <:mk:541268624392978442> Matkultur
+        :hot_pepper: Chili & Lime
+        :pizza: La Fontana 
+        :house: Husman
+        :sushi: Yatai
+        :green_salad: Brödernas
+        :golf: Golfen
+        :office: Collegium
+        :cityscape: LPS
+        :grey_question:  Annat, kommentera!""")
+        else:
+            await message.author.send("DOOF! Lunchvote redan uppe.")
 
     async def background_task(self):
         await self.wait_until_ready()
 
         while not self.is_closed():
             # check if it's time to send the voting message
-            if datetime.datetime.now().weekday() < 6: # and datetime.datetime.now().hour == 9:
+            if datetime.datetime.now().weekday() < 6 and datetime.datetime.now().hour == 8:
                 await self.send_lunch_message()
 
             await asyncio.sleep(60) # task runs every 60 seconds
