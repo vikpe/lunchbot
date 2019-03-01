73f22dcf478973b3733b4d36eda451b7181581cf
stedo <stefan.doverud@gmail.com>
2019-03-01 09:59:37 +0100

    Added functions to read/write lunch msg config from config.json. Added function to test the lunch message in pm: !testlunch Some code cleanup.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import discord
 import asyncio
 import logging
 import datetime
+import json
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.WARNING)
@@ -18,10 +19,26 @@ class MyClient(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self.lunch_message = ""
+        self.last_lunch_message_sent = ""
+        self.config_data = ""
+
         # create the background task and run it in the background
         self.bg_task = self.loop.create_task(self.background_task())
 
+    async def read_config(self):
+        with open('config.json') as json_data_file:
+            self.config_data = json.load(json_data_file)
+        for option in self.config_data["options"]:
+            self.lunch_message = self.config_data["options"][option]["emoji"] + " " + \
+                                 self.config_data["options"][option]["votingOption"] + "\n"
+
+    async def write_config(self):
+        with open('config.json', 'w') as outfile:
+            json.dump(self.confg_data, outfile)
+
     async def on_ready(self):
+        await self.read_config()
         print('Logged in as')
         print(self.user.name)
         print(self.user.id)
@@ -32,27 +49,20 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         if message.content.startswith('!lunch'):
             await self.send_lunch_message(message)
+        if message.content.startswith('!testlunch'):
+            await self.send_test_message(message)
 
     async def send_lunch_message(self, message=None):
         time_delta = datetime.datetime.now() - self.last_lunch_message_sent
         if time_delta.days > 0 or time_delta.seconds >= 12 * 60 * 60:
             self.last_lunch_message_sent = datetime.datetime.now()
             channel = self.get_channel(540608386299985940)
-            await channel.send("""
-:white_check_mark: Ja
-:no_entry: Nej
-<:mk:541268624392978442> Matkultur
-:hot_pepper: Chili & Lime
-:pizza: La Fontana 
-:house: Husman
-:sushi: Yatai
-:green_salad: Brödernas
-:golf: Golfen
-:office: Collegium
-:cityscape: LPS
-:grey_question:  Annat, kommentera!""")
+            await channel.send(self.lunch_message)
         elif message:
             await message.author.send("DOOF! Lunchvote redan uppe.")
+
+    async def send_test_message(self, message=None):
+        await message.author.send(self.lunch_message)
 
     async def background_task(self):
         await self.wait_until_ready()
@@ -62,7 +72,8 @@ class MyClient(discord.Client):
             if datetime.datetime.now().weekday() < 5 and datetime.datetime.now().hour == 8:
                 await self.send_lunch_message()
 
-            await asyncio.sleep(60) # task runs every 60 seconds
+            await asyncio.sleep(60)  # task runs every 60 seconds
+
 
 client = MyClient()
 api_token = os.getenv("LUNCHBOT_TOKEN")
