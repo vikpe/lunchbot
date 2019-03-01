5c20285ce852b4b584afa2801f80a15be9c46d81
stedo <stefan.doverud@gmail.com>
2019-03-01 10:16:26 +0100

    fixed error in parsing of json loop

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ class MyClient(discord.Client):
         with open('config.json') as json_data_file:
             self.config_data = json.load(json_data_file)
         for option in self.config_data["options"]:
-            self.lunch_message = [option]["emoji"] + " " + [option]["votingOption"] + "\n"
+            self.lunch_message = [option]['emoji'] + " " + [option]['votingOption'] + "\n"
         print(self.config_data)
         print(self.lunch_message)
 
