b2c64029ce5d7965fee9c42594dde7ea75c42fd1
Viktor Persson <viktor.persson@arcsin.se>
2020-10-10 13:37:46 +0200

    Refactor.

--- main.py
+++ main.py
@@ -9,13 +9,21 @@ from datetime import datetime
 import discord
 import pytz
 
-logger = logging.getLogger("discord")
-logger.setLevel(logging.WARNING)
-handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
-handler.setFormatter(
-    logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
-)
-logger.addHandler(handler)
+
+class LunchBotConfig:
+    TIMEZONE = "Europe/Stockholm"
+    ANNOUNCEMENT_HOUR = 9
+    CHANNEL_ID = 540608386299985940  # Highly Unprofessional / lunch
+
+
+def setup_logging():
+    logger = logging.getLogger("discord")
+    logger.setLevel(logging.WARNING)
+    handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+    handler.setFormatter(
+        logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
+    )
+    logger.addHandler(handler)
 
 
 class LunchBot(discord.Client):
@@ -24,12 +32,12 @@ class LunchBot(discord.Client):
 
         # init stuff
         self.lunch_message = ""
-        self.config_data = ""
+        self.config = ""
         self.ow_tanks = ""
         self.ow_damage = ""
         self.ow_support = ""
-        self.last_announcement = ""
-        self.announcements = os.getenv("ANNOUNCEMENTS")
+        self.last_announcement_date = ""
+        self.announcements_enabled = os.getenv("ANNOUNCEMENTS")
 
         # create the background task and run it
         self.bg_task = self.loop.create_task(self.background_task())
@@ -37,22 +45,22 @@ class LunchBot(discord.Client):
     async def read_config(self):
         self.lunch_message = ""
         with open("config.json") as json_data_file:
-            self.config_data = json.load(json_data_file)
+            self.config = json.load(json_data_file)
 
         # read lunch options
-        for option in self.config_data["options"]:
+        for option in self.config["options"]:
             self.lunch_message += option["emoji"] + " " + option["votingOption"] + "\n"
 
         # read ow characters
-        self.ow_tanks = self.config_data["owTanks"]
-        self.ow_damage = self.config_data["owDamage"]
-        self.ow_support = self.config_data["owSupport"]
+        self.ow_tanks = self.config["owTanks"]
+        self.ow_damage = self.config["owDamage"]
+        self.ow_support = self.config["owSupport"]
 
     async def write_config(self):
         print("Writing config data")
-        print(self.config_data)
+        print(self.config)
         with open("config.json", "w") as outfile:
-            json.dump(self.config_data, outfile)
+            json.dump(self.config, outfile)
         print("Done")
 
     async def on_ready(self):
@@ -60,7 +68,7 @@ class LunchBot(discord.Client):
         print("Logged in as")
         print(self.user.name)
         print(self.user.id)
-        print("Announcements are " + str(self.announcements))
+        print("Announcements are " + str(self.announcements_enabled))
         print("------")
 
     async def on_message(self, message):
@@ -82,15 +90,17 @@ class LunchBot(discord.Client):
             await self.send_ow_message(message)
 
     async def send_lunch_message(self, message):
-        channel = self.get_channel(540608386299985940)
+        channel = self.get_channel(LunchBotConfig.CHANNEL_ID)
         await channel.send(self.lunch_message)
 
     async def send_test_message(self, message=None):
         await message.author.send(self.lunch_message)
 
     async def get_announcements(self, message):
-        self.announcements = os.getenv("ANNOUNCEMENTS")
-        await message.author.send("Announcements are " + str(self.announcements))
+        self.announcements_enabled = os.getenv("ANNOUNCEMENTS")
+        await message.author.send(
+            "Announcements are " + str(self.announcements_enabled)
+        )
         await message.author.send("This can be changed in the Heroku Config Vars")
 
     async def send_owtank_message(self, message):
@@ -114,22 +124,27 @@ class LunchBot(discord.Client):
         while not self.is_closed():
             # check if announcements are enabled
             print("Checking self.announcements")
-            if self.announcements:
+            if self.announcements_enabled:
                 # check if we already sent a message today
-                print("Checking self.last_announcement")
-                if date.today() != self.last_announcement:
+                print("Checking self.last_announcement_date")
+                todays_date = date.today()
+
+                if todays_date != self.last_announcement_date:
                     # check if it's time to send the voting message
-                    tz_stockholm = pytz.timezone("Europe/Stockholm")
-                    datetime_stockholm = datetime.now(tz_stockholm)
+                    datetime_stockholm = datetime.now(
+                        pytz.timezone(LunchBotConfig.TIMEZONE)
+                    )
 
                     print("Checking weekday and hour")
-                    is_workday = datetime_stockholm.weekday() < 5
-                    if (
-                        is_workday
-                        and datetime_stockholm.hour == 9
-                    ):
+                    is_workingday = datetime_stockholm.weekday() < 5
+                    is_time_for_announcement = (
+                        datetime_stockholm.hour == LunchBotConfig.ANNOUNCEMENT_HOUR
+                    )
+
+                    if is_workingday and is_time_for_announcement:
                         print("Setting self.last_annoucement")
-                        self.last_announcement = date.today()
+                        self.last_announcement_date = todays_date
+
                         print("Sending lunch message")
                         await self.send_lunch_message()
 
@@ -137,7 +152,9 @@ class LunchBot(discord.Client):
             await asyncio.sleep(60)  # task runs every 60 seconds
 
 
-client = LunchBot()
-api_token = os.getenv("LUNCHBOT_TOKEN")
+if __name__ == "__main__":
+    setup_logging()
 
-client.run(api_token)
+    bot = LunchBot()
+    api_token = os.getenv("LUNCHBOT_TOKEN")
+    bot.run(api_token)
